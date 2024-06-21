fc7804ea94b6ba12d891bc5cff0f5a8d19351343
dvtruong-0 <derektruong13@gmail.com>
2024-06-21 12:22:43 -0400

    done

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent} from '@testing-library/react';
+import { render, screen, fireEvent, waitFor} from '@testing-library/react';
 import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 
@@ -19,25 +19,133 @@ afterEach(() => {
 
 
 
- test('test that App component doesn\'t render dupicate Task', () => {
+ test('test that App component doesn\'t render dupicate Task', async () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  //first instance of task
+  fireEvent.change(inputTask, {target : { value : 'duplicate'}});
+  console.log(inputTask.value)
+  fireEvent.change(inputDate, {target : { value : '06/20/2024'}});
+  console.log(inputDate.value)
+  fireEvent.click(element);
+
+  //check that the item is added in the first place
+  let tasks = screen.getAllByText('duplicate');
+  expect(tasks.length).toBe(1);
+
+  //add duplicate task with same date
+  fireEvent.change(inputTask, {target : { value : 'duplicate'}});
+  fireEvent.change(inputDate, {target : { value: '06/20/2024'}});
+
+  fireEvent.click(element);
+
+  //add duplicate task with different date
+  fireEvent.change(inputTask, { target : { value : 'duplicate'}});
+  fireEvent.change(inputDate, {target : { value: '03/20/2025'}});
+  fireEvent.click(element);
+
+  tasks = screen.getAllByText("duplicate");
+  expect(tasks.length).toBe(1);
  });
 
  test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  //trying to add a task without a task name or date
+  fireEvent.click(element);
+
+  expect(screen.queryByText('task')).not.toBeInTheDocument();
+
+  //trying to add a task without a task name but with a date
+  fireEvent.change(inputDate, {target : { value : '06/20/2024'}});
+  fireEvent.click(element);
+
+  expect(screen.queryByText('task')).not.toBeInTheDocument();
  });
 
  test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+
+  //trying to add a task with a task name but wihout a date
+  fireEvent.change(inputTask, {target : { value : 'task'}});
+  fireEvent.click(element);
+
+  expect(screen.queryByText('task')).not.toBeInTheDocument();
  });
 
 
 
  test('test that App component can be deleted thru checkbox', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+
+  //adding multiple elements
+  fireEvent.change(inputTask, {target : { value : 'task1'}});
+  fireEvent.change(inputDate, {target : { value : '06/20/2024'}});
+  fireEvent.click(element);
+
+  fireEvent.change(inputTask, {target : { value : 'task2'}});
+  fireEvent.change(inputDate, {target : { value : '06/20/2024'}});
+  fireEvent.click(element);
+
+  fireEvent.change(inputTask, {target : { value : 'task3'}});
+  fireEvent.change(inputDate, {target : { value : '06/20/2024'}});
+  fireEvent.click(element);
+
+  expect(screen.getByText('task1')).toBeVisible();
+  expect(screen.getByText('task2')).toBeVisible();
+  expect(screen.getByText('task3')).toBeVisible();
+
+  //removing task2
+  const task2Checkbox = screen.getByTestId("checkbox-task2");
+  fireEvent.click(task2Checkbox);
+
+  expect(screen.queryByText('task2')).not.toBeInTheDocument();
+  expect(screen.getByText('task1')).toBeVisible();
+  expect(screen.getByText('task3')).toBeVisible();
+
+  //uncheck all
+  const task1Checkbox = screen.getByTestId("checkbox-task1");
+  const task3Checkbox = screen.getByTestId("checkbox-task3");
+
+  fireEvent.click(task1Checkbox);
+  fireEvent.click(task3Checkbox);
+
+  expect(screen.queryByText('task1')).not.toBeInTheDocument();
+  expect(screen.queryByText('task3')).not.toBeInTheDocument();
+  expect(screen.getByText("You have no todo's left")).toBeVisible();
  });
 
 
  test('test that App component renders different colors for past due events', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+
+  //adding multiple elements
+  //past due date
+  fireEvent.change(inputTask, {target : { value : 'task1'}});
+  fireEvent.change(inputDate, {target : { value : '06/20/2024'}});
+  fireEvent.click(element);
+
+  //future due date
+  fireEvent.change(inputTask, {target : { value : 'task2'}});
+  fireEvent.change(inputDate, {target : { value : '06/25/2026'}});
+  fireEvent.click(element);
+
+  const task1Check = screen.getByTestId('task1').style.background;
+  const task2Check = screen.getByTestId('task2').style.background;
+
+  expect(task1Check).toBe('rgb(255, 105, 97)');
+  expect(task2Check).toBe('rgb(255, 255, 255)');
  });
--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
+import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
@@ -7,7 +9,8 @@ class AddTodo extends Component {
     super();
     this.state = {
       content: "",
-      date: ""
+      date: "",
+      due: null
     };
   }
   // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
@@ -19,6 +22,15 @@ class AddTodo extends Component {
       date: Date().toLocaleString('en-US')
     });
   };
+  // The handleDueDate function updates the react state with what the user inputs for the desired due date of the todo item.
+  // This update/change is donne when the user inputs or changes the due date for a to do list item they plan to add.
+  handleDueDateChange = (event) => {
+    this.setState({
+      date: Date().toLocaleString('en-US'),
+      due: new Date(event).toLocaleString()
+    })
+  }
+
   // The handleSubmit function collects the forms input and puts it into the react state.
   // event.preventDefault() is called to prevents default event behavior like refreshing the browser.
   // this.props.addTodo(this.state) passes the current state (or user input and current date/time) into the addTodo function defined
@@ -29,7 +41,8 @@ class AddTodo extends Component {
       this.props.addTodo(this.state);
       this.setState({
         content: "",
-        date: ""
+        date: "",
+        due: null
       });
     }
   };
@@ -45,15 +58,26 @@ class AddTodo extends Component {
       <div>
         <TextField
           label="Add New Item"
+          data-testid="new-item-input"
           variant="outlined"
           onChange={this.handleChange}
           value={this.state.content}
         />
+        <LocalizationProvider dateAdapter={AdapterDateFns}>         
+          <DesktopDatePicker
+            id="new-item-date"
+            label="Due Date"
+            value={this.state.due}
+            onChange={this.handleDueDateChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
         <Button
           style={{ marginLeft: "10px" }}
           onClick={this.handleSubmit}
           variant="contained"
           color="primary"
+          data-testid="new-item-button"
         >
           Add
         </Button>
